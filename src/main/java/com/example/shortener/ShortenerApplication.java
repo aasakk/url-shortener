b65b7c8b03e6d959d1d070a9ca4cfff852841d3d
Dev Ops <khireaasavari@gmail.com>
2025-07-04 19:03:27 +0530

    changed ShortenerApplication.java

--- src/main/java/com/example/shortener/ShortenerApplication.java
+++ src/main/java/com/example/shortener/ShortenerApplication.java
@@ -2,18 +2,11 @@ package com.example.shortener;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import com.codahale.metrics.MetricRegistry;
 
 @SpringBootApplication
 public class ShortenerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ShortenerApplication.class, args);
-    }
-    @Bean
-    public MetricRegistry metricRegistry() {
-        return new MetricRegistry();
     }
 }
 
